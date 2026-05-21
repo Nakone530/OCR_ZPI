a7e274e0bcc53dc09dda752e5c306b9cd5291365
Nakone530 <nk14530@student.ans-ns.edu.pl>
2026-05-21 16:47:23 +0200

    Dodanie oceny kształtu lini

--- test_txtline.py
+++ test_txtline.py
@@ -125,12 +125,7 @@ def split_lines_from_roi(roi, x, y, median_height, min_peak_distance=10):
         for s, e in segments
     ]
 
-def detect_text_lines(image_path):
-    img = cv2.imread(image_path)
-
-    if img is None:
-        raise ValueError(f"Nie można wczytać obrazu: {image_path}")
-    
+def detect_text_words(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Binaryzacja
@@ -172,9 +167,8 @@ def detect_text_lines(image_path):
 
     median_height = int(np.median(heights))
     median_width = int(np.median(widths))
-    most_safe_bb = []
     too_large = []
-    lines = []
+    words = []
     for cnt in contours:
         
         x, y, w, h = cv2.boundingRect(cnt)
@@ -183,19 +177,13 @@ def detect_text_lines(image_path):
         if 5 + 0.2 * median_height <= h <= 3.0 * median_height:
             print(" ")
             if w > 5:
-                lines.append((x, y, w, h))
+                words.append((x, y, w, h))
         else:
             too_large.append((x, y, w, h))
 
             roi = thresh[y:y+h, x:x+w]
             print("TOO LARGE:", (x, y, w, h))
             split = split_lines_from_roi(roi, x, y, median_height)
-##            split = [
-##                (bx, by, bw, bh)
-##                for (bx, by, bw, bh) in split
-##                if bw > 5 and bh > 5 + 0.3 * median_height
-##            ]
-##            lines.extend(split)
             too_small = [
                 (bx, by, bw, bh) 
                 for (bx, by, bw, bh) in split
@@ -211,38 +199,245 @@ def detect_text_lines(image_path):
             center_x = x + w / 2
             
             
-            #lines.extend(too_small)
-            lines.extend(most_safe)
+            #words.extend(too_small)
+            words.extend(most_safe)
         
     # Sortowanie od góry do dołuedian_heig
-    lines = sorted(lines, key=lambda b: b[1])
+    words = [
+        Word(i, box)
+        for i, box in enumerate(words)
+    ]
+    
+    words.sort(key=lambda w: w.y)
 
     # Kopia do rysowania
     output = img.copy()
 
-    for i, (x, y, w, h) in enumerate(lines):
-        # Zielony prostokąt
+    for i, word in enumerate(words):
+        
+        #Zielony prostokąt
         cv2.rectangle(
             output,
-            (x, y),
-            (x + w, y + h),
+            (word.x, word.y),
+            (word.x + word.w, word.y + word.h),
             (0, 255, 0),
             2
         )
-
-        # Numer linii
+        #Numer lini
         cv2.putText(
             output,
-            f"{i}",
-            (x, y - 10),
+            f"{word.idx}",
+            (word.x, word.y - 10),
             cv2.FONT_HERSHEY_SIMPLEX,
             0.7,
             (0, 0, 255),
             2
         )
 
-    return output, thresh, dilated, lines
+    return output, thresh, dilated, words, output.copy()
 
+class Word:
+    def __init__(self, idx, box):
+        self.idx = idx
+
+        self.x, self.y, self.w, self.h = box
+
+        self.cx = self.x + self.w / 2
+        self.cy = self.y + self.h / 2
+
+        self.used = False
+
+    @property
+    def left(self):
+        return self.x
+
+    @property
+    def right(self):
+        return self.x + self.w
+
+    @property
+    def top(self):
+        return self.y
+
+    @property
+    def bottom(self):
+        return self.y + self.h
+
+    @property
+    def center(self):
+        return (int(self.cx), int(self.cy))
+    
+    @property
+    def box(self):
+        return (self.x, self.y, self.w, self.h)
+    
+    def __iter__(self):
+        return iter((self.x, self.y, self.w, self.h))
+
+    def __repr__(self):
+        return (
+            f"Word(idx={self.idx}, "
+            f"x={self.x}, y={self.y}, "
+            f"w={self.w}, h={self.h})"
+        )
+
+def candidate_neighbors(word, words, median_h):
+
+    candidates = []
+
+    for other in words:
+
+        if other.idx == word.idx:
+            continue
+
+        dx = other.cx - word.cx
+        dy = other.cy - word.cy
+
+        if dx <= 0:
+            continue
+
+        if dx > median_h * 15:
+            continue
+
+        if abs(dy) > median_h * 1.5:
+            continue
+
+        candidates.append((other, dx, dy))
+
+    return candidates
+
+def neighbor_score(word, other):
+
+    dx = other.cx - word.cx
+    dy = other.cy - word.cy
+
+    slope = abs(dy / max(dx, 1))
+
+    height_ratio = abs(other.h - word.h) / max(word.h, 1)
+
+    score = (
+        abs(dy) * 2.0 +
+        slope * 30.0 +
+        dx * 0.1 +
+        height_ratio * 25.0
+    )
+
+    return score
+
+def best_neighbor(word, words, median_h):
+
+    candidates = candidate_neighbors(
+        word,
+        words,
+        median_h
+    )
+
+    if not candidates:
+        return None
+
+    scored = []
+
+    for other, dx, dy in candidates:
+
+        score = neighbor_score(word, other)
+
+        scored.append((score, other))
+
+    scored.sort(key=lambda x: x[0])
+
+    return scored[0][1]
+
+def grow_line(start_word, words, median_h):
+
+    line = [start_word]
+
+    current = start_word
+
+    current.used = True
+
+    while True:
+
+        nxt = best_neighbor(
+            current,
+            words,
+            median_h
+        )
+
+        if nxt is None:
+            break
+
+        if nxt.used:
+            break
+
+        nxt.used = True
+
+        line.append(nxt)
+
+        current = nxt
+
+    return line
+
+
+def detect_text_lines(img, output, words):
+    lines = []
+    true_l  = img.copy()
+    false_l = img.copy()
+    heights = [w.h for w in words]
+    median_h = np.median(heights)
+    for word in words:
+
+        if word.used:
+            continue
+
+        line = grow_line(
+            word,
+            words,
+            median_h
+        )
+
+        if len(line) > 0:
+            lines.append(line)
+
+    for line in lines:
+
+        for i in range(len(line) - 1):
+
+            w1 = line[i]
+            w2 = line[i + 1]
+            
+            pc1 = (int(w1.cx), int(w1.cy))
+            pc2 = (int(w2.cx), int(w2.cy))
+            
+            y1 = int(w1.cy)
+            y2 = int(w2.cy)
+
+
+            p1 = (int(w1.x), y1)
+            p2 = (int(w1.x + w1.w), y1)
+
+            cv2.line(output, p1, p2, (0, 0, 255), 2)
+            cv2.line(img, p1, p2, (0, 0, 255), 2)
+            cv2.line(false_l, p1, p2, (0, 0, 255), 2)
+            
+            p3 = (int(w1.x + w1.w), y1)
+            p4 = (int(w2.x), y2)
+
+            cv2.line(output, p3, p4, (0, 0, 255), 2)
+            cv2.line(img, p3, p4, (0, 0, 255), 2)
+            cv2.line(false_l, p3, p4, (0, 0, 255), 2)
+
+            p5 = (int(w2.x), y2)
+            p6 = (int(w2.x + w2.w), y2)
+
+            cv2.line(output, p5, p6, (255, 0, 0), 2)
+            cv2.line(img, p5, p6, (255, 0, 0), 2)
+            cv2.line(false_l, p5, p6, (0, 0, 255), 2)
+
+            cv2.line(output, pc1, pc2, (255, 0, 0), 2)
+            cv2.line(img, pc1, pc2, (255, 0, 0), 2)
+            cv2.line(true_l, pc1, pc2, (255, 0, 0), 2)
+            
+    return img, output, true_l, false_l, lines
 
 
 if __name__ == "__main__":
@@ -273,21 +468,24 @@ if __name__ == "__main__":
         exit(1)
 
     for image_path in image_paths:
-
+        
         print(f"\n=== {image_path} ===")
+        original = cv2.imread(image_path)
 
-        output, thresh, dilated, lines = \
-            detect_text_lines(image_path)
+        if original is None:
+            raise ValueError(f"Nie można wczytać obrazu: {image_path}")
+        
+        output, thresh, dilated, words, w_output = detect_text_words(original.copy())
 
-        print(f"Znaleziono {len(lines)} linii")
+        l_output, output, true_l_output, false_l_output, lines  = detect_text_lines(original.copy(), output, words)        
+        print(f"Znaleziono {len(words)} linii")
 
-        for i, (x, y, w, h) in enumerate(lines):
+        for i, (x, y, w, h) in enumerate(words):
             print(f"{i}: x={x}, y={y}, w={w}, h={h}")
 
         if not args.debug:
             continue
 
-        original = cv2.imread(image_path)
 
         cc_vis, _, _ = visualize_connected_components(thresh)
 
@@ -306,6 +504,10 @@ if __name__ == "__main__":
 
         debug_views = [
             ("Output", output),
+            ("Wyrazy", w_output),
+            ("Linie", l_output),
+            ("Prawdziwe Linie", true_l_output),
+            ("Fałszywe Linie", false_l_output),
             ("Original", original),
             ("Binaryzacja", thresh_bgr),
             ("Dylatacja", dilated_bgr),
